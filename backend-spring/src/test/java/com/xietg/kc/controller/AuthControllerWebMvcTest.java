fac24ac652cf8e8b5c11ac45e846d979b8fda919
Guillaume Teixeron <guillaume.teixeron@gmail.com>
2026-03-07 11:49:41 +0100

    fix test dependencies issues

--- backend-spring/src/test/java/com/xietg/kc/controller/AuthControllerWebMvcTest.java
+++ backend-spring/src/test/java/com/xietg/kc/controller/AuthControllerWebMvcTest.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.boot.webmvc.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 
 import java.util.Optional;
 
