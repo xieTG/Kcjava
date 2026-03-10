db45fbe8db3ac9a7eebb4fa15594261f66d4be40
Guillaume Teixeron <guillaume.teixeron@gmail.com>
2026-03-10 10:55:05 +0100

    fix(test) and ensure DataJpaTest is available

--- backend-spring/src/test/java/com/xietg/kc/db/repo/QuestionnaireRepositoryDataJpaTest.java
+++ backend-spring/src/test/java/com/xietg/kc/db/repo/QuestionnaireRepositoryDataJpaTest.java
@@ -4,7 +4,7 @@ import com.xietg.kc.db.entity.QuestionnaireEntity;
 import com.xietg.kc.integration.AbstractPostgresIT;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
